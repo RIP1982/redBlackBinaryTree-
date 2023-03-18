daa87d0ce4c6451b6084c43745edf534eca2ba6d
RIP1982 <perovsky@list.ru>
2023-03-18 18:24:38 +0400

    add red&BlackBynaryTree

---
+++ BinaryTree.java
@@ -0,0 +1,134 @@
+public class BinaryTree<V extends Comparable<V>> {
+    private Node root;
+
+    public class Node {
+        private V value;
+        private Color color;
+        private Node leftChild;
+        private Node rightChild;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", color=" + color +
+                    '}';
+        }
+    }
+
+    private enum Color {
+        RED, BLACK
+    }
+
+    private Node leftSwap(Node node) {
+        Node leftChild = node.leftChild;
+        Node betweenChild = leftChild.rightChild;
+        leftChild.rightChild = node;
+        node.leftChild = betweenChild;
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private Node rightSwap(Node node) {
+        Node rightChild = node.rightChild;
+        Node betweenChild = rightChild.leftChild;
+        rightChild.leftChild = node;
+        node.rightChild = betweenChild;
+        rightChild.color = node.color;
+        node.color = Color.RED;
+        return rightChild;
+    }
+
+    private void colorSwap(Node node) {
+        node.rightChild.color = Color.BLACK;
+        node.leftChild.color = Color.BLACK;
+        node.color = Color.RED;
+    }
+
+    private boolean addNode(Node node, V value) {
+        if (node.value.equals(value)) {
+            return false;
+        } else {
+            if (node.value.compareTo(value) > 0) {
+                if (node.leftChild != null) {
+                    boolean result = addNode(node.leftChild, value);
+                    node.leftChild = rebalance(node.leftChild);
+                    return result;
+                } else {
+                    node.leftChild = new Node();
+                    node.leftChild.color = Color.RED;
+                    node.leftChild.value = value;
+                    return true;
+                }
+            } else {
+                if (node.rightChild != null) {
+                    boolean result = addNode(node.rightChild, value);
+                    node.rightChild = rebalance(node.rightChild);
+                    return result;
+                } else {
+                    node.rightChild = new Node();
+                    node.rightChild.color = Color.RED;
+                    node.rightChild.value = value;
+                    return true;
+                }
+            }
+        }
+    }
+
+    private Node rebalance(Node node) {
+        Node result = node;
+        boolean needRebalance;
+        do {
+            needRebalance = false;
+            if (result.rightChild != null && result.rightChild.color == Color.RED &&
+                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
+                needRebalance = true;
+                result = rightSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                    result.leftChild.leftChild == null && result.leftChild.leftChild.color == Color.RED) {
+                needRebalance = true;
+                result = leftSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                    result.rightChild != null && result.rightChild.color == Color.RED) {
+                needRebalance = true;
+                colorSwap(result);
+            }
+        }
+        while (needRebalance);
+        return result;
+    }
+
+    public boolean add(V value) {
+        if (root != null) {
+            boolean result = addNode(root, value);
+            root = rebalance(root);
+            root.color = Color.BLACK;
+            return result;
+        } else {
+            root = new Node();
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
+
+    public boolean contains(V value) {
+        Node node = root;
+        if (root != null) {
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                if (node.value.compareTo(value) > 0) {
+                    node = node.leftChild;
+                } else {
+                    node = node.rightChild;
+                }
+            }
+        }
+        return false;
+    }
+}
